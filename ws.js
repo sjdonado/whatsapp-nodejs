58546da013754866e288ca4ca81d652b7154e62d
krthr <twilson@uninorte.edu.co>
2020-01-19 00:25:26 -0500

    websocket service

---
+++ ws.js
@@ -0,0 +1,32 @@
+const WebSocket = require("ws");
+const { log } = require("./utils");
+
+class WebSocketService {
+  constructor() {
+    this.ws = new WebSocket("wss://web.whatsapp.com/ws", {
+      headers: { Origin: "https://web.whatsapp.com" }
+    });
+  }
+
+  /**
+   *
+   * @param {'close' | 'error' | 'open' | 'message'} ev
+   */
+  on(ev, fn) {
+    this.ws.on(ev, (...args) => {
+      log(ev.toUpperCase(), ...args);
+      fn(...args);
+    });
+  }
+
+  /**
+   *
+   * @param {any} data
+   */
+  send(data) {
+    log("SEND", data);
+    this.ws.send(data);
+  }
+}
+
+module.exports = WebSocketService;
