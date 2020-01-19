2fe36baa2e371da9efb2cdfa5715b38a3c50b585
krthr <twilson@uninorte.edu.co>
2020-01-19 00:25:13 -0500

    qr to file

--- utils.js
+++ utils.js
@@ -1,7 +1,14 @@
+const qr = require("qrcode");
+
 const log = (...args) => {
   console.log(args.join(" ::: "));
 };
 
+const qrToFile = (txt = "") => {
+  qr.toFile("./qr.png", txt, console.log);
+};
+
 module.exports = {
-  log
+  log,
+  qrToFile
 };
